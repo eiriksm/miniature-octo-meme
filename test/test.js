2abf2fca07a94085081095d425a780d21f98a3a4
Eirik S. Morland <eirik@morland.no>
2014-10-08 17:53:03 +0200

    Add server.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 require('should');
 var a = require('..');
+var b = require('../server');
 
 describe('Awesome module', function() {
   it('Should return the expected value', function() {
@@ -8,4 +9,18 @@ describe('Awesome module', function() {
   it('Should throw error when given bad values', function() {
     a.bind().should.throw();
   });
+});
+
+describe('Awesome server', function() {
+  it('Should respond as expected', function(done) {
+    require('supertest')(b)
+    .get('/gtoa/1.050/1.010')
+    .end(function(e, r, b) {
+      r.statusCode.should.equal(200);
+      parseFloat(r.text, 10).toFixed(2).should.equal('5.24');
+      done(e);
+    });
+
+  });
+
 });
